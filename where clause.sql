49b0a03bab9da1517eb3029b58440025e8eebbd1
abidladha <abidop397@gmail.com>
2026-02-01 15:17:49 +0500

    Add files via upload

---
+++ where clause.sql
@@ -0,0 +1,8 @@
+---where clause---
+---where clause helps us to filter the data based on a condition.
+select * from agriculture_dataset
+where Fertilizer_Used_tons >= 5
+--- it helps us to retrieve data based on a condition
+--- we observed here that we can even compare an integer to a floating number in this case
+--- operators that can be used in we clause
+---   (>) , (<) , (>=) , (<=) , (=) , (!=)--> not equal to 
