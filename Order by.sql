d470801c20dd819ec9a2096f2bc59614b81b612b
abidladha <abidop397@gmail.com>
2026-02-22 16:39:21 +0500

    Add files via upload

---
+++ Order by.sql
@@ -0,0 +1,6 @@
+SELECT * FROM customers Where score>500
+ORDER BY score asc
+
+SELECT * FROM customers Where score<500 and country='Germany'
+--DER BY score 
+--From a worldwide test,send me the combined score of all the students from Germany
